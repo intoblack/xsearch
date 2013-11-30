559b0ff0e7808767c20b382783ac68ead44fa49e
lixuze <intoblack86@gmail.com>
2013-11-30 12:40:33 +0800

    添加httpclient 包装类 添加测试代码

--- src/main/java/com/liran/zero/http/Test.java
+++ src/main/java/com/liran/zero/http/Test.java
@@ -1,7 +1,21 @@
 package com.liran.zero.http;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("1");
+	public static void main(String[] args) throws ClientProtocolException,
+			IOException {
+		HttpGet get = new HttpGet("http://news.baidu.com");
+		HttpResponse response = ZeroHttpClient.httpClient.execute(get);
+		HttpEntity resEntity = response.getEntity();
+		System.out.println((resEntity == null) ? null : EntityUtils
+				.toString(resEntity));
+		resEntity.consumeContent();
 	}
 }
--- src/main/java/com/liran/zero/http/ZeroHttpClient.java
+++ src/main/java/com/liran/zero/http/ZeroHttpClient.java
@@ -1,31 +1,57 @@
 package com.liran.zero.http;
 
 import org.apache.http.HttpVersion;
-import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 
 public class ZeroHttpClient {
+
+	private static final String CHARSET = HTTP.UTF_8;
+	public static final DefaultHttpClient httpClient = getMutlDefaultHttpClient();
+
+	private ZeroHttpClient() {
+
+	}
 
 	@SuppressWarnings("deprecation")
 	public static DefaultHttpClient getMutlDefaultHttpClient() {
 		HttpParams params = new BasicHttpParams();
-		ConnManagerParams.setMaxTotalConnections(params, 100);
+		// 设置一些基本参数
 		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-		SchemeRegistry schemeRegistry = new SchemeRegistry();
-		schemeRegistry.register(new Scheme("http", PlainSocketFactory
+//		HttpProtocolParams.setContentCharset(params, CHARSET);
+		HttpProtocolParams.setUseExpectContinue(params, true);
+		HttpProtocolParams.setUserAgent(params,
+				USER_AGENT.UBUNTU_CHROMIUM_30_0.getUserAgent());
+		// 超时设置
+		// 从连接池中取连接的超时时间
+		ConnManagerParams.setTimeout(params, 1000);
+		//设置链接时间
+		HttpConnectionParams.setConnectionTimeout(params, 2000);
+		//设置超时时间
+		HttpConnectionParams.setSoTimeout(params, 4000);
+		//设置最大链接数目
+		ConnManagerParams.setMaxTotalConnections(params, 200);
+		// 设置我们的HttpClient支持HTTP和HTTPS两种模式
+		SchemeRegistry schReg = new SchemeRegistry();
+		schReg.register(new Scheme("http", PlainSocketFactory
 				.getSocketFactory(), 80));
-		ClientConnectionManager cm = new ThreadSafeClientConnManager(params,
-				schemeRegistry);
-		DefaultHttpClient httpClient = new DefaultHttpClient(cm, params);
-		return httpClient;
+		schReg.register(new Scheme("https",
+				SSLSocketFactory.getSocketFactory(), 443));
+
+		// 使用线程安全的连接管理来创建HttpClient
+		ClientConnectionManager conMgr = new ThreadSafeClientConnManager(
+				params, schReg);
+		return new DefaultHttpClient(conMgr, params);
 	}
 }
