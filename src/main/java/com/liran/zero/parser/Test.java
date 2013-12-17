5d29a42a258e1dbf73198313b9ff3824232c4359
lixuze <intoblack86@gmail.com>
2013-12-17 22:36:44 +0800

    提交修改

--- src/main/java/com/liran/zero/parser/Test.java
+++ src/main/java/com/liran/zero/parser/Test.java
@@ -8,8 +8,8 @@ import org.jsoup.select.Elements;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-		Elements newsHeadlines = doc.select("#mp-itn b a");
+		Document doc = Jsoup.connect("http://www.baidu.com").get();
+		Elements newsHeadlines = doc.select("#nv > a");
 		System.out.println(newsHeadlines.text());
 		
 	}
